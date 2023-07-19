871bc3479e8239fffa82d2f62172b86d4b93dc56
Alex43961 <semen43961@gmail.com>
2023-07-19 10:38:24 +0300

    "added"

---
+++ JS/modal.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Получить модальный
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content")
+// const modalClasses = modal.classList;
+
+// Получить кнопку, которая открывает модальный
+const btn = document.getElementById("delete-btn");
+
+// Получить кнопку "Close", который закрывает модальный
+ const closeModal = document.getElementsByClassName("close")[0];
+closeModal.addEventListener("click", () => {
+animatedCloseModal();    
+});
+
+function animatedCloseModal() {
+  let result = modalContent.classList.add("close-modal");
+     
+    setTimeout(function(){
+      modalContent.classList.remove("close-modal");
+      modal.style.display = "none";
+    }, 400);
+}
+
+
+const btnModalClose = document.getElementById("btn-modal-close");
+const btmModalDelete = document.getElementById("btn-modal-delete");
+
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+
+// Когда пользователь нажимает на кнопку "Close", закройте модальное окно
+ btnModalClose.addEventListener("click", () => {
+  animatedCloseModal();  ;
+});
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.addEventListener("click",(event) => {
+  if (event.target == modal) {
+    animatedCloseModal();  ;
+  }
+});
+
+btmModalDelete.addEventListener("click",() =>{
+  localStorage.removeItem("all-chats");
+  loadDataFromLocalStorage();
+});
+
